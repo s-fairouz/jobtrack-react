f88766aebb87dddec3a990eb8a1fec7644aeea2a
s-fairouz <95185316+s-fairouz@users.noreply.github.com>
2026-09-15 16:51:41 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-blue-800 text-4xl p-4 m-4 text-center">Start Coding in React 19</h1>
+    </>
+  );
+}
+
+export default App;
