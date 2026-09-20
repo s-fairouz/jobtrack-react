333ce43ae34c4ff29fc4d2e45bf4520d216efe21
fairouz salem <sfairouz5@gmail.com>
2026-09-20 15:52:14 +0100

    Fix import order in eslint.config.js: move reactCompiler import above globals

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import js from "@eslint/js";
-import globals from "globals";
+import reactCompiler from "eslint-plugin-react-compiler";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
 
 export default defineConfig([
   globalIgnores(["dist"]),
